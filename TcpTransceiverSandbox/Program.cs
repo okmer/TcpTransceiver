8ede0cf65f5e96756703edf83ce412a47b7fdeeb
Remko van Brakel <remko@okmer.com>
2018-08-18 22:13:35 +0200

    Import simple Tcp Transceiver sandbox.

--- TcpTransceiverSandbox/Program.cs
+++ TcpTransceiverSandbox/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+using Com.Okmer.Communication;
 
 namespace TcpTransceiverSandbox
 {
@@ -6,7 +12,33 @@ namespace TcpTransceiverSandbox
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tcp Transceiver Sandbox");
+
+            var host = new TcpTransceiverHost(9999);
+            host.Start();
+
+            Task hostWriter = Task.Run(() =>
+            {
+                int counter = 0;
+                while (true)
+                {
+                    host.WriteData(Encoding.ASCII.GetBytes($"Test text number {counter++}{Environment.NewLine}"));
+                }
+            });
+
+            var client = new TcpTransceiverClient(IPAddress.Loopback, 9999);
+            client.Connect();
+
+            Task clientReader = Task.Run(() =>
+            {
+                while (true)
+                {
+                    byte[] data = client.ReadData(1024);
+                    Console.WriteLine(Encoding.ASCII.GetString(data));
+                }
+            });
+
+            Console.ReadLine();
         }
     }
 }
